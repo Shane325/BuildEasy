6a460effd4cdef5c011a47719c024bda6396b1b4
Shane325 <Shane325@gmail.com>
2016-03-05 11:58:59 -0800

    added angularMoment lib into the app. I will use this lib for date and time functions.

--- app/js/services.js
+++ app/js/services.js
@@ -497,16 +497,16 @@ angular.module('myApp.services', [])
     var timelineServiceObject = {
         saveNewTask: function(newTask, projectId){
             
-        var originDate = new Date('03/01/2016');
-        var enteredDate = new Date(newTask.startDate);
-        var miliseconds = enteredDate - originDate;
-        var seconds = miliseconds/1000;
-        var minutes = seconds/60;
-        var hours = minutes/60;
-        var days = hours/24;
-        console.log(days);
+//            var originDate = new Date('03/01/2016');
+//            var enteredDate = new Date(newTask.startDate);
+//            var miliseconds = enteredDate - originDate;
+//            var seconds = miliseconds/1000;
+//            var minutes = seconds/60;
+//            var hours = minutes/60;
+//            var days = hours/24;
+//            console.log(days);
             
-//            projectTimeline.$child(projectId).$child('taskList').$add(newTask);
+            projectTimeline.$child(projectId).$child('taskList').$add(newTask);
         },
         getTimelineTasksByProject: function(projectId){
             return projectTimeline.$child(projectId).$child('taskList');
